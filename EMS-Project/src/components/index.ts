f4e4bf338cfa953718d344d6c225e6b573d86184
ZuheirAl26 <zuheiralhomsi73@gmail.com>
2026-07-23 00:27:36 +0300

    Adding global components

---
+++ EMS-Project/src/components/index.ts
@@ -0,0 +1,7 @@
+export { Button } from "./Button";
+export { ConfirmDialog } from "./ConfirmDialog";
+export { EmptyState } from "./EmptyState";
+export { Input } from "./Input";
+export { Loader } from "./Loader";
+export { Modal } from "./Modal";
+export { Table } from "./Table";
